26488baebff0d5b25c15092a1bf91d47a9abaa44
chengmarc <last@live.ca>
2023-09-11 18:30:53 -0400

    name change

---
+++ execute.bat
@@ -0,0 +1,5 @@
+call trade_scan_stock_us.py
+call trade_scan_stock_ca.py
+call trade_scan_stock_hk.py
+call trade_scan_stock_zh.py
+pause
--- trade-scan execute.bat
+++
@@ -1,5 +0,0 @@
-call stock-market-zh.py
-call stock-market-hk.py
-call stock-market-ca.py
-call stock-market-us.py
-pause
