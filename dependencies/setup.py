b989ed2fc68c0a0ee92fe5e61850636a3130adbc
Cheng <uzcheng@intranet.rexel.ca>
2023-10-25 14:37:57 -0400

    updated dependencies

--- dependencies/setup.py
+++ dependencies/setup.py
@@ -10,5 +10,5 @@ setup(
     name='tsl-dependencies',
     version='1.1',
     packages=find_packages(),
-    install_requires=['tk', 'selenium', 'pandas==2.1.0', 'bs4', 'colorama']
+    install_requires=['tk==0.1.0', 'selenium==4.9.0', 'pandas==2.1.0', 'bs4==0.0.1', 'colorama==0.4.6']
 )
